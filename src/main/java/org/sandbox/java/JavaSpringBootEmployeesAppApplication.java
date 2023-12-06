d0b26f1b7a691b32c1d3d6d083d8d6f1cd41082a
Uladzislau S <36772576+uladzislau-slk@users.noreply.github.com>
2023-12-06 22:50:11 +0100

    Initial commit

---
+++ src/main/java/org/sandbox/java/JavaSpringBootEmployeesAppApplication.java
@@ -0,0 +1,13 @@
+package org.sandbox.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootEmployeesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBootEmployeesAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sandbox/java/JavaSpringBootEmployeesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sandbox.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootEmployeesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
